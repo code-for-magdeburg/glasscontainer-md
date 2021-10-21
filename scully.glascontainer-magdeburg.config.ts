4ce4020da03d8ba5a45054abd5ef9200a2bab411
Dave Foksa <dave.foksa@de.ibm.com>
2021-10-21 22:21:23 +0200

    add scully

---
+++ scully.glascontainer-magdeburg.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "glascontainer-magdeburg",
+  outDir: './dist/static',
+  routes: {
+  }
+};
